52a2711b5ff2ea13334aa3bb06a631e1fdb50282
Lucas Valério <97574936+Lucas-Monteiro07@users.noreply.github.com>
2022-11-25 15:39:05 -0300

    feat coverage

--- test/FolhaDePagamento.test.js
+++ test/FolhaDePagamento.test.js
@@ -1,8 +1,19 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
 import { somaHorasExtras, calculaDescontos } from '../index';
 
-test('Deve retornar a soma das horas extras', () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
+describe('Testes dos cálculos da folha de pagamento', () => {
+  it('Deve retornar a soma das horas extras', () => {
+    const esperado = 2500;
+    const retornado = somaHorasExtras(2000, 500);
 
-  expect(retornado).toBe(esperado);
+    expect(retornado).toBe(esperado);
+  });
+
+  it('Deve calcular o valor do Salário', () => {
+    const esperado = 2300;
+    const retornado = calculaDescontos(2500, 200);
+
+    expect(retornado).toBe(esperado);
+  });
 });
